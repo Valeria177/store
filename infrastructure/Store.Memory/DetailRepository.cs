f86be6d945deca848a3c66991794b54d9f7413ea
Valeria <levchenkovalerya@yandex.ru>
2020-11-10 20:33:09 +0300

    Добавить страницу поиска

--- infrastructure/Store.Memory/DetailRepository.cs
+++ infrastructure/Store.Memory/DetailRepository.cs
@@ -5,15 +5,17 @@ namespace Store.Memory
 {
     public class DetailRepository : IDetailRepository
     {
-        private readonly Detail[] detail = new[]
+        private readonly Detail[] details = new[]
         {
             new Detail(1, "Ремонтный к-т направляющей суппорта"),
             new Detail(2, "Диск тормозной задний"),
             new Detail(3, "Колодки дисковые"),
         };
+
         public Detail[] GetAllByTitle(string titlePart)
         {
-            return detail.Where(detail => detail.Title.Contains(titlePart)).ToArray();
+            return details.Where(detail => detail.Title.Contains(titlePart)).ToArray();
+
         }
     }
 }
---
+++ presentation/Store.Web/Controllers/SearchController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Store.Web.Controllers
+{
+    public class SearchController : Controller
+    {
+        private readonly IDetailRepository detailRepository;
+
+        public SearchController(IDetailRepository detailRepository)
+        {
+            this.detailRepository = detailRepository;
+        }
+        public IActionResult Index(string querty)
+        {
+            var details = detailRepository.GetAllByTitle(querty);
+
+            return View(details);
+        }
+    }
+}
