9b22b30a5340280b2f9cab59e567e051573522ca
Valeria <levchenkovalerya@yandex.ru>
2020-11-10 15:26:57 +0300

    Добавить реализацию репозитория деталей в памяти

---
+++ infrastructure/Store.Memory/DetailRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace Store.Memory
+{
+    public class DetailRepository : IDetailRepository
+    {
+        private readonly Detail[] detail = new[]
+        {
+            new Detail(1, "Ремонтный к-т направляющей суппорта"),
+            new Detail(2, "Диск тормозной задний"),
+            new Detail(3, "Колодки дисковые"),
+        };
+        public Detail[] GetAllByTitle(string titlePart)
+        {
+            return detail.Where(detail => detail.Title.Contains(titlePart)).ToArray();
+        }
+    }
+}
